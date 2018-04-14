19660a4315afd5a3e857776ad2b807bf7610b0f2
Piotr Polanski <polanski.piotr@me.com>
2018-04-14 19:46:10 +0000

    gen_tcp and Task

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -7,9 +7,10 @@ defmodule KVServer.Application do
 
   def start(_type, _args) do
     # List all child processes to be supervised
+    port = String.to_integer(System.get_env "PORT" || raise "missing $PORT env variable")
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg},
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
