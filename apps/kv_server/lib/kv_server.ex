4dd071a324acc802f00013c505a3fce1777c1f66
Piotr Polanski <polanski.piotr@me.com>
2018-04-15 12:23:14 +0000

    kv server integration tests

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -10,6 +10,7 @@ defmodule KVServer do
   end
 
   defp loop_acceptor socket do
+    Logger.info "in loop_acceptor"
     {:ok, client} = :gen_tcp.accept socket
     {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
     :ok = :gen_tcp.controlling_process client, pid
@@ -17,19 +18,35 @@ defmodule KVServer do
   end
 
   defp serve socket do
-    socket
-    |> read_line()
-    |> write_line(socket)
+    Logger.info "in serve"
+    msg = 
+      with {:ok, data} <- read_line(socket),
+           {:ok, command} <- KVServer.Command.parse(data),
+            do: KVServer.Command.run(command)
 
+
+    write_line socket, msg
     serve socket
   end
 
   defp read_line socket do
-    {:ok, data} = :gen_tcp.recv socket, 0
-    data
+    :gen_tcp.recv socket, 0
   end
 
-  defp write_line line, socket do
-    :gen_tcp.send socket, line
+  defp write_line socket, {:ok, text} do
+    :gen_tcp.send socket, text
   end
+
+  defp write_line socket, {:error, :unknown_command} do
+    :gen_tcp.send socket, "UNKNOWN COMMAND\r\n"
+  end
+
+  defp write_line socket, {:error, :closed} do
+    exit :shutdown
+  end
+
+  defp write_line socket, {:error, :not_found} do
+    :gen_tcp.send socket, "NOT FOUND\r\n"
+  end
+
 end
